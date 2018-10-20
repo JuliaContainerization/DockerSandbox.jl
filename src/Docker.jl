18d62aad9b3a064b87edec3c69b9dddf609b8832
Raphael P. Ribeiro <raphaelpr01@gmail.com>
2018-10-20 13:18:16 -0300

    julia 0.7+ required changes

--- src/Docker.jl
+++ src/Docker.jl
@@ -1,25 +1,25 @@
 module Docker
 
-using Requests
+using HTTP
 using JSON
 using Compat
 
-immutable DockerError
+struct DockerError
     status::Int
     msg::Compat.String
 end
 
 const headers = Dict("Content-Type" => "application/json")
 
-docker_uri(host) = URI("http://$host/v1.21")
-docker_uri(host,endpoint) = URI("http://$host/v1.21/$endpoint")
-parse(data) = JSON.parse(join(map(Char,data)))
+docker_uri(host) = HTTP.URI("http://$host/v1.21")
+docker_uri(host,endpoint) = HTTP.URI("http://$host/v1.21/$endpoint")
+parse_body(data) = JSON.parse(join(map(Char,data)))
 
 function create_container(
         host, image;
         cmd::Cmd     = ``,
         entryPoint   = "",
-        tty          = true,
+        tty_stream   = true,
         attachStdin  = false,
         openStdin    = false,
         attachStdout = true,
@@ -29,14 +29,14 @@ function create_container(
         volumeDriver = "",
         portBindings = ["",""], # [ContainerPort,HostPort]
         ports        = [],
-        pwd          = ""
+        workingDir   = ""
     )
 
     url = docker_uri(host)
 
     params = Dict(
         "Image" => image,
-        "Tty" => tty,
+        "Tty" => tty_stream,
         "AttachStdin"   => attachStdin,
         "OpenStdin"     => openStdin,
         "AttachStdout"  => attachStdout,
@@ -62,104 +62,108 @@ function create_container(
         params["Cmd"] = collect(cmd.exec)
     end
 
-    if !isempty(pwd)
-        params["WorkingDir"] = pwd
+    if !isempty(workingDir)
+        params["WorkingDir"] = workingDir
     end
 
-    resp = post(URI("$url/containers/create"),json=params,headers=headers)
+    resp = HTTP.request("POST",HTTP.URI("$url/containers/create"),[],JSON.json(params),headers=headers)
     if resp.status != 201
-        throw(DockerError(resp.status,resp.data))
+        throw(DockerError(resp.status,resp.body))
     end
-    parse(resp.data)
+    parse_body(resp.body)
 end
 
 function inspect_container(host,id)
-    resp = get(docker_uri(host,"containers/$id/json"))
+    resp = HTTP.request("GET",(docker_uri(host,"containers/$id/json")))
     if resp.status != 200
-        throw(DockerError(resp.status,resp.data))
+        throw(DockerError(resp.status,resp.body))
     end
-    parse(resp.data)
+    parse_body(resp.body)
 end
 
 function start_container(host, id)
-    resp = post(docker_uri(host,"containers/$id/start"))
+    resp = HTTP.request("POST",(docker_uri(host,"containers/$id/start")))
+    print(resp)
     if resp.status != 204
-        throw(DockerError(resp.status,resp.data))
+        throw(DockerError(resp.status,resp.body))
     end
     id
 end
 
 function restart_container(host, id)
-    resp = post(docker_uri(host,"containers/$id/restart"))
+    resp = HTTP.request("POST",(docker_uri(host,"containers/$id/restart")))
     if resp.status != 204
-        throw(DockerError(resp.status,resp.data))
+        throw(DockerError(resp.status,resp.body))
     end
     resp
 end
 
 function stop_container(host, id)
-    resp = post(docker_uri(host,"containers/$id/stop"))
+    resp = HTTP.request("POST",(docker_uri(host,"containers/$id/stop")))
     if resp.status != 204
-        throw(DockerError(resp.status,resp.data))
+        throw(DockerError(resp.status,resp.body))
     end
     resp
 end
 
 function pause_container(host, id)
-    resp = post(docker_uri(host,"containers/$id/pause"))
+    resp = HTTP.request("POST",(docker_uri(host,"containers/$id/pause")))
     if resp.status != 204
-        throw(DockerError(resp.status,resp.data))
+        throw(DockerError(resp.status,resp.body))
     end
     resp
 end
 
 function unpause_container(host, id)
-    resp = post(docker_uri(host,"containers/$id/unpause"))
+    resp = HTTP.request("POST",(docker_uri(host,"containers/$id/unpause")))
     if resp.status != 204
-        throw(DockerError(resp.status,resp.data))
+        throw(DockerError(resp.status,resp.body))
     end
     id
 end
 
 function kill_container(host, id)
-    resp = post(docker_uri(host,"containers/$id/kill"),"")
+    resp = HTTP.request("POST",(docker_uri(host,"containers/$id/kill")))
     if resp.status != 204
-        throw(DockerError(resp.status,resp.data))
+        throw(DockerError(resp.status,resp.body))
     end
     resp
 end
 
 function remove_container(host, id)
-    resp = Requests.delete(docker_uri(host,"containers/$id?force=1"))
+    resp = HTTP.request("DELETE",(docker_uri(host,"containers/$id?force=1")))
     if resp.status != 204
-        throw(DockerError(resp.status,resp.data))
+        throw(DockerError(resp.status,resp.body))
     end
     resp
 end
 
 function processes_container(host, id)
-    resp = get(docker_uri(host,"containers/$id/top"))
-    println(resp.status)
-    if resp.status != 200
-        throw(DockerError(resp.status,resp.data))
+    resp = HTTP.request("GET",(docker_uri(host,"containers/$id/top")))
+    if resp.status != 204
+        println(resp.status)
     end
-    parse(resp.data)
+    if resp.status != 200
+        throw(DockerError(resp.status,resp.body))
+    end
+    parse_body(resp.body)
 end
 
 function list_containers(host)
-    resp = get(docker_uri(host,"containers/json"))
+    resp = HTTP.request("GET",(docker_uri(host,"containers/json")))
+    print(resp)
     if resp.status != 200
-        throw(DockerError(resp.status,resp.data))
+        throw(DockerError(resp.status,resp.body))
     end
-    parse(resp.data)
+    parse_body(resp.body)
 end
 
 function stats_container(host,id)
-    resp = get(docker_uri(host,"containers/$id/stats?stream=0"))
+    resp = HTTP.request("GET",(docker_uri(host,"containers/$id/stats?stream=0")))
     if resp.status != 200
-        throw(DockerError(resp.status,resp.data))
+        throw(DockerError(resp.status,resp.body))
     end
-    parse(resp.data)
+    parse_body(resp.body)
 end
 
 function open_logs_stream(host, id; history=false)
@@ -168,15 +172,15 @@ function open_logs_stream(host, id; history=false)
         path *=  "&logs=1"
     end
     url = docker_uri(host,path)
-    Requests.open_stream(url,[Dict("Content-Type"=>"plain/text")],"","POST")
+    HTTP.open(url,"GET",[Dict("Content-Type"=>"plain/text")],"","POST")
 end
 
 function cleanse!(host)
-    resp = get(docker_uri(host,"containers/json?all=true"))
+    resp = HTTP.request("GET",(docker_uri(host,"containers/json?all=true")))
     if resp.status != 200
-        throw(DockerError(resp.status,resp.data))
+        throw(DockerError(resp.status,resp.body))
     end
-    data = parse(resp.data)
+    data = parse_body(resp.body)
     for c in data
         remove_container(host,c["Id"])
         kill_container(host,c["Id"])
@@ -184,4 +188,4 @@ function cleanse!(host)
     nothing
 end
 
-end
+end # module Docker
