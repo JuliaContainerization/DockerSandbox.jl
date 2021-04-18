5cac49cba7ad8bc223bbf51aa03e77f9ea911fca
Dilum Aluthge <dilum@aluthge.com>
2021-04-17 21:10:51 -0400

    Update types.jl

--- src/types.jl
+++ src/types.jl
@@ -6,17 +6,17 @@ $(DocStringExtensions.TYPEDFIELDS )
 """
 Base.@kwdef struct DockerConfig
     image::String
+    verbose::Bool                         = false
     env::Dict{String, String}             = Dict{String, String}()
     platform::Symbol                      = :linux
     read_only_maps::Dict{String, String}  = Dict{String, String}()
     read_write_maps::Dict{String, String} = Dict{String, String}()
-    stderr::IO                            = Base.stderr
-    stderr_docker_build::IO               = Base.stderr
     stdin::IO                             = Base.devnull
-    stdin_docker_build::IO                = Base.devnull
     stdout::IO                            = Base.stdout
+    stderr::IO                            = Base.stderr
+    stdin_docker_build::IO                = Base.devnull
     stdout_docker_build::IO               = Base.stdout
-    verbose::Bool                         = false
+    stderr_docker_build::IO               = Base.stderr
 end
 
 """
