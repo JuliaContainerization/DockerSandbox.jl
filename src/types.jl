c11aa0e4d63f184aa7a4b5aa21808fb270bb7dd9
Dilum Aluthge <dilum@aluthge.com>
2021-04-18 20:39:45 -0400

    Add read-only maps (#36)

--- src/types.jl
+++ src/types.jl
@@ -8,21 +8,22 @@
 - `verbose::Bool = false`
 - `env::Dict{String, String} = Dict{String, String}()`
 - `platform::Symbol = :linux`
-- `read_only_maps::Dict{String, String} = Dict{String, String}()`
-- `read_write_maps::Dict{String, String} = Dict{String, String}()`
+- `read_only_maps::Union{Dict{String, String}, Nothing} = nothing`
+- `read_write_maps::Union{Dict{String, String}, Nothing} = nothing`
 - `stdin::IO = Base.devnull`
 - `stdout::IO = Base.stdout`
 - `stderr::IO = Base.stderr`
 - `docker_build_stdout::Union{IO, Nothing} = nothing`
 - `docker_build_stderr::Union{IO, Nothing} = nothing`
+
 """
 Base.@kwdef struct DockerConfig
     image::String
     verbose::Bool = false
     env::Dict{String, String} = Dict{String, String}()
     platform::Symbol = :linux
-    read_only_maps::Dict{String, String} = Dict{String, String}()
-    read_write_maps::Dict{String, String} = Dict{String, String}()
+    read_only_maps::Union{Dict{String, String}, Nothing} = nothing
+    read_write_maps::Union{Dict{String, String}, Nothing} = nothing
     stdin::IO = Base.devnull
     stdout::IO = Base.stdout
     stderr::IO = Base.stderr
---
+++ src/utils.jl
@@ -0,0 +1,35 @@
+"""
+    apply_chmod_recursively(root_dir::String; dir_mode::Integer, file_mode::Integer)
+"""
+function apply_chmod_recursively(root_dir::String;
+                                 dir_mode::Integer,
+                                 file_mode::Integer)
+    chmod(root_dir, dir_mode)
+    for (root, dirs, files) in walkdir(root_dir)
+        for dir in dirs
+            full_dir_path = joinpath(root, dir)
+            isdir(full_dir_path) || throw(ArgumentError("unexpected error"))
+            chmod(full_dir_path, dir_mode)
+        end
+        for file in files
+            full_file_path = joinpath(root, file)
+            isfile(full_file_path) || throw(ArgumentError("unexpected error"))
+            chmod(full_file_path, file_mode)
+        end
+    end
+    return nothing
+end
+
+"""
+    make_world_readable_recursively(root_dir::String)
+"""
+function make_world_readable_recursively(root_dir::String)
+    return apply_chmod_recursively(root_dir; dir_mode=0o555, file_mode=0o444)
+end
+
+# """
+#     make_world_writeable_recursively(root_dir::String)
+# """
+# function make_world_writeable_recursively(root_dir::String)
+#     return apply_chmod_recursively(root_dir; dir_mode=0o666, file_mode=0o777)
+# end
