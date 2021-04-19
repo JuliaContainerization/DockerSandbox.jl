6282d05680517a447630787bc7c3659dcd61b427
Dilum Aluthge <dilum@aluthge.com>
2021-04-18 23:07:29 -0400

    Ability to add Linux capabilities

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,8 +8,10 @@ include("utils.jl")
 
 include("no_nonstdlib_deps.jl")
 include("docker_is_installed.jl")
+
+include("add_capabilities.jl")
 include("environment_variables.jl")
 include("errors.jl")
-include("with_container.jl")
 include("read_only_maps.jl")
 include("read_write_maps.jl")
+include("with_container.jl")
