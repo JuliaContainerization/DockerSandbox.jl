fd89b56c74b265cf10aba579d7c77d6783136c41
Dilum Aluthge <dilum@aluthge.com>
2021-04-20 20:01:33 -0400

    Allow the `with_container` function to take keyword arguments, which it will forward to the constructor for `T`

--- src/convenience.jl
+++ src/convenience.jl
@@ -1,16 +1,10 @@
 """
-    with_container(f::Function, ::Type{T} = DockerContainer) where {T <: DockerContainer}
+    with_container(f::Function, ::Type{<:DockerContainer} = DockerContainer; kwargs...)
 """
-function with_container(f::Function,
-                        ::Type{T} = DockerContainer) where {T <: DockerContainer}
-    container = T()
-    return with_container(f, container)
-end
-
-"""
-    with_container(f::Function, container::DockerContainer)
-"""
-function with_container(f::Function, container::DockerContainer)
+function with_container(f::F,
+                        ::Type{T} = DockerContainer;
+                        kwargs...) where {F <: Function, T <: DockerContainer}
+    container = T(; kwargs...)
     try
         return f(container)
     finally
